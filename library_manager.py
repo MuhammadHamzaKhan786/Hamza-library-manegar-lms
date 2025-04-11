95d7075dca7d998d2f9b39bab5de82560df010c1
Muhammad Hamza Khan <anwarhamza712@gmail.com>
2025-04-11 15:57:12 +0500

    library_manager.py

--- library_manager.py
+++ library_manager.py
@@ -6,7 +6,7 @@ import datetime
 import requests
 import time
 import plotly.express as px
-# import plotly.graph_objects as go
+ import plotly.graph_objects as go
 from streamlit_lottie import st_lottie
 import requests
 
