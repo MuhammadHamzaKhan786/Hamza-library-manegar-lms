24efdeaff6129ce9e46fb582411ce1141855d759
Muhammad Hamza Khan <anwarhamza712@gmail.com>
2025-04-19 01:18:17 +0500

    library_manager.py

--- library_manager.py
+++ library_manager.py
@@ -49,7 +49,8 @@ st.markdown("""
         border-radius: 0.375rem;
     }
     .book-card {
-        background-color: #F3F4F6;
+        background-color:#1E3A8A;
+         font-weight: bold;
         border-radius: 0.5rem;
         padding: 1rem;
         margin-bottom: 1rem;
